4abc47251808e30e0b9402893dedc79cff7f5415
Muhammad-Nasyr <nasirmaga45@gmail.com>
2022-10-19 00:01:49 +0300

    adding some functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
+  const [date, setDate] = useState(false);
+
+  const partialDate = (
+    <div>
+      Bugün açık 09:00 – 17:00{" "}
+      <span onClick={() => setDate(true)} className="material-symbols-outlined">expand_more</span>
+    </div>
+  );
+
+  const fullDate = (
+    <div>
+      <span onClick={() => setDate(false)} className="material-symbols-outlined">expand_less</span>
+    </div>
+  );
+
   return (
     <footer>
       <hr />
       <p className={styles.footTitle}>Bize Ulaşın</p>
+
       <p className={styles.regularText}>
         Daha da iyisi, gelin, yüz yüze görüşelim!
       </p>
+
       <p className={styles.lightText}>
         Müşterilerimize değer veriyoruz. Mesai saatleri içinde bizi dilediğiniz
         zaman ziyaret edebilirsiniz.
       </p>
+
       <p className={styles.regularText}>PEYK</p>
+
       <a className={styles.email} href="mailto:akhmadov.omar@std.izu.edu.tr">
         akhmadov.omar@std.izu.edu.tr
       </a>
+
       <p className={styles.regularText}>Çalışma Saatleri</p>
+
+      {date ? fullDate : partialDate}
+
       <p className={styles.lightText}>
         Telif Hakkı © 2022 PEYK - Tüm Hakları Saklıdır.
       </p>
